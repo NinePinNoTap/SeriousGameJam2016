2a6add43db2b59d450ccfed3775a2a8fa2dc6b03
unknown <1100223@MC10801.uad.ac.uk>
2016-02-23 10:58:42 +0000

    Signed-off-by: unknown <1100223@MC10801.uad.ac.uk>

---
+++ Assets/Scripts/Buttons/ContactPhoneProvider.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ContactPhoneProvider : MonoBehaviour
+{
+	[Header("Components")]
+	public Text buttonText;
+
+	[Header("Flags")]
+	public bool isActivated;
+
+	void Start()
+	{
+		isActivated = false;
+		buttonText.text = "Contact CSP";
+	}
+
+	public void Toggle()
+	{
+		// Toggle flag
+		isActivated = !isActivated;
+
+		// Set text
+		if(isActivated)
+		{
+			Activate();
+		}
+		else
+		{
+			Deactivate();
+		}
+	}
+
+	private void Activate()
+	{
+	}
+
+	private void Deactivate()
+	{
+	}
+}
